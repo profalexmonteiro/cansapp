e5ed2c11731ad077f8747818c917a01357acb0ed
Alex Monteiro <alex.monteiro@ifam.edu.br>
2024-04-04 00:22:45 -0300

    wifiscan

--- app/src/main/java/com/example/app01/Home.java
+++ app/src/main/java/com/example/app01/Home.java
@@ -16,6 +16,9 @@ import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiSsid;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,6 +29,7 @@ import android.view.View;
 import android.widget.EditText;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Home extends AppCompatActivity implements LocationListener {
 
@@ -50,45 +54,51 @@ public class Home extends AppCompatActivity implements LocationListener {
         etPWL = (EditText) findViewById(R.id.editTextPowerLevel);
 
         try {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
 
-
-    public int getBandwith(){
+    public int getBandwith() {
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        int downSpeed=0;
+        int downSpeed = 0;
         int upSpeed;
 
         //for (Network network : connMgr.getAllNetworks()) {
 
-            //NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
+        //NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
 
-            NetworkCapabilities nc = connMgr.getNetworkCapabilities(connMgr.getActiveNetwork());
-            downSpeed = downSpeed + nc.getLinkDownstreamBandwidthKbps();
-            upSpeed = nc.getLinkUpstreamBandwidthKbps();
+        NetworkCapabilities nc = connMgr.getNetworkCapabilities(connMgr.getActiveNetwork());
+        downSpeed = downSpeed + nc.getLinkDownstreamBandwidthKbps();
+        upSpeed = nc.getLinkUpstreamBandwidthKbps();
 
         //}
 
         return downSpeed;
     }
 
-    public void getVelocidade(){
+    public void getVelocidade() {
         try {
             locationManager = (LocationManager) getApplication().getApplicationContext().getSystemService(LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,Home.this);
-        }catch (Exception e){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, Home.this);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public boolean getStateDisplay(){
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        etSpeed.setText(String.valueOf(location.getSpeed()));
+        Log.d("VELOCIDADE", String.valueOf(location.getSpeed()));
+    }
+
+    public boolean getStateDisplay() {
 
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         boolean isScreenOn = pm.isInteractive();
@@ -96,7 +106,7 @@ public class Home extends AppCompatActivity implements LocationListener {
         return isScreenOn;
     }
 
-    public float getLevelPower(){
+    public float getLevelPower() {
 
         IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         Intent batteryStatus = context.registerReceiver(null, ifilter);
@@ -104,13 +114,34 @@ public class Home extends AppCompatActivity implements LocationListener {
         int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
 
-        float batteryPct = level * 100 / (float)scale;
+        float batteryPct = level * 100 / (float) scale;
 
         return batteryPct;
     }
 
+    public void scanWifi() {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        List<ScanResult> results = wifiManager.getScanResults();
+
+        for (ScanResult result: results){
+            Log.d("SSID: ",result.SSID);
+        }
+
+    }
+
     public void updateParameters(View v){
 
+        scanWifi();
         getVelocidade();
     //    etSpeed.setText(Float.toString(getSpeedMove()));
         etBW.setText(Integer.toString(getBandwith()));
@@ -122,9 +153,6 @@ public class Home extends AppCompatActivity implements LocationListener {
 
     }
 
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-            etSpeed.setText(String.valueOf(location.getSpeed()));
-            Log.d("VELOCIDADE",String.valueOf(location.getSpeed()));
-    }
+
+
 }
